dc6bff956fce7c987475318267739378a7adde07
chrismwendt <chrismwendt@gmail.com>
2012-09-08 15:25:49 -0400

    added a fish image, constantly move, rotate, and draw fish to screen

--- src/Visualizer.java
+++ src/Visualizer.java
@@ -1,14 +1,24 @@
 import javax.swing.JFrame;
 import java.util.Scanner;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.RenderingHints;
+import java.awt.Color;
 
-public class Visualizer implements Runnable {
-    private JFrame jFrame;
+public class Visualizer extends JFrame implements Runnable {
+    private BufferedImage buffer;
+    private Graphics2D bufferGraphics;
     private Engine fishtank;
     private Thread engineThread;
+    private State state;
+    private int width = 512, height = 512;
 
     public Visualizer() {
         //Set up the fishtank
-        jFrame = new JFrame("Fish Tank");
+        buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        bufferGraphics = (Graphics2D)buffer.getGraphics();
+        bufferGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         fishtank = new Engine();
         engineThread = new Thread(fishtank);
     }
@@ -20,26 +30,33 @@ public class Visualizer implements Runnable {
 
     public void run() {
         //Set up visualization window
-        jFrame.setVisible(true);
-        jFrame.setSize(512, 512);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setSize(buffer.getWidth(), buffer.getHeight());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Start the engine
         engineThread.start();
 
         //Repeatedly display the simulation
         while(engineThread.isAlive()) {
-            draw(fishtank.getState());
+            state = fishtank.getState();
+            repaint();
         }
     }
 
     //Push the visualization of the state to the window
-    private void draw(State s) {
+    public void paint(Graphics g) {
         //Draw the tank
-
-        //Draw the plants
-
+        bufferGraphics.setColor(Color.BLUE);
+        bufferGraphics.fillRect(0, 0, buffer.getWidth(), buffer.getHeight());
+        
         //Draw the fish
+        bufferGraphics.setColor(Color.ORANGE);
+        bufferG
+        
+        // TODO Draw the plants
+
+        g.drawImage(buffer, 0, 0, this);
     }
 
     public static void main(String[] argv) {
