1e739b4fd3c31281d51ecd752bf382099f255221
Dan Roberts <droberts@purdue.edu>
2012-10-20 14:46:32 -0400

    Made circlefish notice when a fish has died

--- src/control/CircleFish.java
+++ src/control/CircleFish.java
@@ -1,5 +1,7 @@
 package control;
 
+import java.util.ArrayList;
+
 import environment.Engine;
 import environment.Fish;
 import environment.FishState;
@@ -23,9 +25,14 @@ public class CircleFish extends FishAI {
 			prev = current;
 			current = engine.getState(prev.seqID);
 			
+			ArrayList<Fish> removeList = new ArrayList<Fish>();
 			for (Fish f : myFish) {
 				System.out.println("Handling fish " + f.getID() + " Speed is " + speed);
 				FishState fs = current.get_state(f);
+				if (!fs.isAlive()) {
+					removeList.add(f);
+					continue;
+				}
 				
 				f.setSpeed(speed);
 				if (fs.getPosition().x > engine.rules.x_width - 10) {
@@ -33,6 +40,10 @@ public class CircleFish extends FishAI {
 				} else if (fs.getPosition().x < 10) {
 					f.setRudderDirection(1, 0);
 				}
+			}
+			// Get rid of dead fish
+			for (Fish f : removeList) {
+				myFish.remove(f);
 			}
 			
 			// Set the speed for the next round
