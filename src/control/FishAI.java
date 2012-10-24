53b4a46a5b638d5c024b0cbf90f9d327050fdf0a
Dan Roberts <droberts@purdue.edu>
2012-10-24 00:46:08 -0400

    Implemented the run method in FishAI which calls iterate in subclass AIs.  This allows us to let the thread die when all the fish are dead, although it does not ensure that iterate finishes in a timely manner. Also added a color field to FishAI, which the visualizer uses to graphically differentiate different controllers.

--- src/control/FishAI.java
+++ src/control/FishAI.java
@@ -1,21 +1,42 @@
 package control;
+import java.awt.Color;
 import java.util.Vector;
 
 import environment.Engine;
 import environment.Fish;
+import environment.WorldState;
 
 public abstract class FishAI implements Runnable {
 	public Vector<Fish> myFish = null;
-    Engine engine;
+    public Engine engine;
+    public Color color = new Color(255, 100, 0);
     
     // Represents the number fish the AI will start with
     // Initial nutrients will be divided among all fish
-    public static int startFish;
+    public int startFish;
     
     public FishAI (Engine engine, int startFish) {
     	this.startFish= startFish;
     	this.engine = engine;
     	this.myFish = new Vector<Fish>();
+    }
+    
+    public void run() {
+    	while (liveFish() > 0) {
+    		// TODO: Figure out how to time this, so that we can break out if needed 
+    		iterate();
+    	}
+    }
+    
+    protected abstract void iterate();
+    
+    private int liveFish() {
+    	WorldState st = engine.getState(0);
+    	int numFish = 0;
+    	for (Fish f : myFish) {
+    		if (st.getState(f).isAlive()) numFish++;
+    	}
+    	return numFish;
     }
 }
 
