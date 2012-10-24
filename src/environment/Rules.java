c34c2deb3b04fed12bac02c96481dcb8c3494af6
Dan Roberts <droberts@purdue.edu>
2012-10-24 00:48:45 -0400

    Fish decay is now a function in Rules.  Did some tuning here, found a set of definitions that aren't completely terrible.

--- src/environment/Rules.java
+++ src/environment/Rules.java
@@ -4,6 +4,17 @@ public class Rules {
 	public static final int maxSpeed = 5;
 	public static final int xWidth = 1024;
 	public static final int yWidth = 1024;
-	public static final int minFish = 5;
-	public static final int startingNutrients = 100;
+	public static final int minFish = 15;
+	public static final double startingNutrients = 2000.0;
+	
+	public static final double TIME_DECAY = .05;
+	public static final double SPEED_DECAY = .1;
+	public static final double SIZE_DECAY = .0003;
+	
+	public static double decay(FishState fs) {
+		double decay = TIME_DECAY;
+		decay += SPEED_DECAY * fs.getSpeed();
+		decay += SIZE_DECAY * fs.getNutrients();
+		return decay;
+	}
 }
