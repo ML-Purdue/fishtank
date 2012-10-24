e0e918be57819788e745fa019fa88887bde9ae71
Dan Roberts <droberts@purdue.edu>
2012-10-23 20:03:41 -0400

    Got rid of underscores in exchange for camel case in WorldState

--- src/environment/WorldState.java
+++ src/environment/WorldState.java
@@ -4,23 +4,23 @@ import java.util.Set;
 
 public class WorldState {
     public long seqID;
-    protected java.util.Hashtable<Fish, FishState> fish_states;
+    protected java.util.Hashtable<Fish, FishState> fishStates;
 
     public double xDim;
     public double yDim;
     
-    public Set<Fish> get_fish() {
-    	return fish_states.keySet();
+    public Set<Fish> getFish() {
+    	return fishStates.keySet();
     }
     
-    public FishState get_state (Fish f) {
-    	return fish_states.get(f);
+    public FishState getState (Fish f) {
+    	return fishStates.get(f);
     }
 
     public WorldState(long ID) {
         seqID = ID;
 
-        fish_states = new java.util.Hashtable<Fish, FishState>();
+        fishStates = new java.util.Hashtable<Fish, FishState>();
     }
 }
 
