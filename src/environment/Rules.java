c6e50330cda8cd71015adc96ab73b505396dd771
Christopher Cole <cole14@purdue.edu>
2012-11-03 14:47:04 -0400

    Remove duplicate maxSpeed variable, and use the correct one.

--- src/environment/Rules.java
+++ src/environment/Rules.java
@@ -1,7 +1,6 @@
 package environment;
 
 public class Rules {
-	public static final int maxSpeed = 20;
 	public static final int tankWidth = 1024;
 	public static final int tankHeight = 1024;
 	public static final int minFish = 15;
@@ -12,7 +11,7 @@ public class Rules {
 	public static final double SIZE_DECAY = .001;
 	
 	public static final double MAX_NUTRIENTS = startingNutrients;
-	public static final double MAX_SPEED = 5; 
+	public static final double MAX_SPEED = 20; 
 	
 	public static double decay(FishState fs) {
 		double decay = TIME_DECAY;
