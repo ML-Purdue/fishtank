6958a784d4c3a1de5ccc1ee7337301275c87682b
chrismwendt <chrismwendt@gmail.com>
2012-11-17 12:40:23 -0500

    food respawns at a rate rather than instantly

--- src/environment/Engine.java
+++ src/environment/Engine.java
@@ -27,7 +27,7 @@ public class Engine implements Runnable {
 	private int typeIndex = 0;
 	private int spawnRequest = 0;
 	private int numFish = 0;
-	private int foodCount = 10;
+	private int foodRate = 30;
 	private Hashtable<Integer, Color> fishColors;
 	private boolean hyperspeed = false;
 	private Visualizer visualizer;
@@ -48,7 +48,6 @@ public class Engine implements Runnable {
 		this.visualizer = visualizer;
 
 		flipStates();
-		generateFood(foodCount);
 	}
 	
 	public void printState() {
@@ -102,13 +101,6 @@ public class Engine implements Runnable {
     	}
     }
 
-	
-	private void generateFood(int n) {
-		for (int i = 0; i < n; i++) {
-			backState.food.add(new Food(new Vector(rng.nextDouble() * Rules.tankWidth, rng.nextDouble() * Rules.tankHeight), 100));
-		}
-	}
-
 	private void moveFish() {
 		for (FishAI ai : controllers) {
 			synchronized(ai.myFish) {
@@ -151,12 +143,15 @@ public class Engine implements Runnable {
 	
 	private void eatFood() {
 		for (FishState fishState : backState.fishStates.values()) {
-			for (Food food : backState.food) {
+			for (int i = 0; i < backState.food.size();) {
+				Food food = backState.food.get(i);
 				double dist = Math.sqrt(Math.pow((fishState.position.x - food.position.x), 2)
 						+ Math.pow((fishState.position.y - food.position.y), 2));
 				if(dist < fishState.getRadius() + food.getRadius()){
 					fishState.nutrients += (food.nutrients * 0.8);
-					food.position = new Vector(rng.nextDouble() * Rules.tankWidth, rng.nextDouble() * Rules.tankHeight);
+					backState.food.remove(food);
+				} else {
+					i++;
 				}
 			}
 		}
@@ -363,6 +358,10 @@ public class Engine implements Runnable {
 			backState.mousePosition = visualizer.mousePosition.clone();
 
 			// Calculate the next state from frontState into the backState
+			if (backState.seqID % foodRate == 0) {
+				backState.food.add(new Food(new Vector(rng.nextDouble() * Rules.tankWidth, rng.nextDouble() * Rules.tankHeight), 100));
+			}
+			
 			moveFish();
 			
 			eatFood();
